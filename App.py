f1ea0d2b767da7b55abf6562367ede3d531d8713
juliocesar06 <julio06cesar11@gmail.com>
2021-11-23 22:12:05 -0300

    app

---
+++ App.py
@@ -0,0 +1,19 @@
+#1 importar bibliotecas
+import pywhatkit
+import keyboard
+import time
+from datetime import date, datetime
+#2 definir contatos
+
+# lista de contatos
+contatos = ["""colocar os contatos aqui dentro separando por virgulas ex +5531987654321,"""]
+
+#3 definir intervalo
+i = 0
+while len(contatos) >= 1:
+    pywhatkit.sendwhatmsg(contatos[i],'ultimo teste...',datetime.now().hour,datetime.now().minute+1)
+    del contatos[i]
+    time.sleep(30)
+    keyboard.press_and_release('ctrl + w')
+    i += 1
+#4 testar
